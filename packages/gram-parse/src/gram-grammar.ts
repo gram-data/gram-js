eceb07ecfc040193dda9b57d3db14fbd35b3e079
Andreas Kollegger <akollegger@gmail.com>
2020-12-09 00:33:11 +0100

    build(all) bumped dependency versions

--- packages/gram-parse/src/gram-grammar.ts
+++ packages/gram-parse/src/gram-grammar.ts
@@ -1,4 +1,4 @@
-// Generated automatically by nearley, version 2.19.8
+// Generated automatically by nearley, version 2.20.1
 // http://github.com/Hardmath123/nearley
 // Bypasses TS6133. Allow declared but unused functions.
 // @ts-ignore
@@ -114,7 +114,7 @@ interface NearleyLexer {
   reset: (chunk: string, info: any) => void;
   next: () => NearleyToken | undefined;
   save: () => any;
-  formatError: (token: NearleyToken) => string;
+  formatError: (token: never) => string;
   has: (tokenType: string) => boolean;
 }
 
